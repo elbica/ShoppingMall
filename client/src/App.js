1b6313d44d47e19130475d13d2ce74c4173d8489
luceinaltis <luceinaltis2020@naver.com>
2021-06-14 23:03:33 +0900

    kihwan

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,69 @@
-import "./App.css"
-import React, { useState, useEffect } from "react"
-import { BrowserRouter, Route } from "react-router-dom"
-import Home from "./Pages/Home"
-import Login from "./Pages/Login"
-import Nav from "./Components/Nav"
-import Board from "./Pages/Board"
-import Product from "./Pages/Product"
-import Register from "./Pages/Register"
-import BoardView from "./Pages/BoardView"
-import Profile from "./Pages/Profile"
-import axios from "axios"
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Nav from "./Components/Nav";
+import Board from "./Pages/Board";
+import Product from "./Pages/Product";
+import Register from "./Pages/Register";
+import BoardView from "./Pages/BoardView";
+import Profile from "./Pages/Profile";
+import Cart from "./Pages/Cart";
+import axios from "axios";
 function App() {
-  let [userState, setUserState] = useState({
-    loginCheck: false,
-    id: "",
-    nickName: "",
-  })
-  // const changeState = (n, v) => {
-  //   setUserState({ [n]: v })
-  // }
-  useEffect(() => {
-    axios
-      .post("/")
-      .then(res => {
-        setUserState({
-          id: res.data.id,
-          nickName: res.data.nickName,
-          loginCheck: res.data.loginCheck,
-        })
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
+    let [userState, setUserState] = useState({
+        loginCheck: false,
+        id: "",
+        nickName: "",
+    });
+    // const changeState = (n, v) => {
+    //   setUserState({ [n]: v })
+    // }
 
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <div className="nav">
-          <Nav Version={userState.loginCheck} />
-        </div>
+    useEffect(() => {
+        axios
+            .post("/")
+            .then((res) => {
+                setUserState({
+                    id: res.data.id,
+                    nickName: res.data.nickName,
+                    loginCheck: res.data.loginCheck,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-        <Route path="/" exact component={Home} />
-        <Route path="/login" exact render={() => <Login change={setUserState} />} />
-        <Route path="/board/:page" exact component={Board} />
-        <Route path="/board/:page/:id" exact component={BoardView} />
-        <Route path="/product" exact component={Product} />
-        <Route path="/register" exact component={Register} />
-        <Route
-          path="/profile"
-          exact
-          render={props => <Profile array={userState} change={setUserState} {...props} />}
-        />
-        {/*
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <div className="nav">
+                    <Nav Version={userState.loginCheck} />
+                </div>
+
+                <Route path="/" exact component={Home} />
+                <Route path="/login" exact render={() => <Login change={setUserState} />} />
+                <Route path="/board/:page" exact component={Board} />
+                <Route path="/board/:page/:id" exact component={BoardView} />
+                <Route path="/product" exact component={Product} />
+                <Route path="/register" exact component={Register} />
+                <Route
+                    path="/profile"
+                    exact
+                    render={(props) => (
+                        <Profile array={userState} change={setUserState} {...props} />
+                    )}
+                />
+                <Route path="/cart" exact component={Cart} />
+                {/*
         <Route path="/cart" exact component={}></Route>
       <Route path="/admin" exact component={}></Route> 
       */}
-      </div>
-    </BrowserRouter>
-  )
+            </div>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
---
+++ client/src/Pages/Cart.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+function Cart() {
+    const [ad, setad] = useState();
+
+    return <div>안뇽</div>;
+}
+
+export default Cart;
