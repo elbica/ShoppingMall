73e2d6ce7e4836a74f41e4fed1ab69db90bd8821
luceinaltis <luceinaltis2020@naver.com>
2021-06-20 17:14:55 +0900

    Product Detail init

--- client/src/Pages/Product.jsx
+++ client/src/Pages/Product.jsx
@@ -1,70 +1,70 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import Image from "../Components/Image"
-import "../css/Product.css"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Image from "../Components/Image";
+import "../css/Product.css";
 
 let models = [
-  {
-    src: "images/image.png",
-    title: "test",
-    price: 123,
-    review: 12,
-    width: 14,
-  },
-  {
-    src: "images/image.png",
-    title: "test",
-    price: 123,
-    review: 12,
-    width: 14,
-  },
-  {
-    src: "images/image.png",
-    title: "test",
-    price: 123,
-    review: 12,
-    width: 14,
-  },
-  {
-    src: "images/image.png",
-    title: "test",
-    price: 123,
-    review: 12,
-    width: 14,
-  },
-  {
-    src: "images/image.png",
-    title: "test",
-    price: 123,
-    review: 12,
-    width: 14,
-  },
-]
+    {
+        src: "images/image.png",
+        title: "test",
+        price: 123,
+        review: 12,
+        width: 14,
+    },
+    {
+        src: "images/image.png",
+        title: "test",
+        price: 123,
+        review: 12,
+        width: 14,
+    },
+    {
+        src: "images/image.png",
+        title: "test",
+        price: 123,
+        review: 12,
+        width: 14,
+    },
+    {
+        src: "images/image.png",
+        title: "test",
+        price: 123,
+        review: 12,
+        width: 14,
+    },
+    {
+        src: "images/image.png",
+        title: "test",
+        price: 123,
+        review: 12,
+        width: 14,
+    },
+];
 const Product = (props) => {
-  return (
-    <div className="product_box">
-      <Image {...props}></Image>
-      <div className="product_content">
-        <p>{props.title}</p>
-        <p>{props.price}</p>
-        <p>{props.review}</p>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="product_box">
+            <Image {...props}></Image>
+            <div className="product_content">
+                <p>{props.title}</p>
+                <p>{props.price}</p>
+                <p>{props.review}</p>
+            </div>
+        </div>
+    );
+};
 export default () => {
-  return (
-    <div className="products">
-      <div className="products_admin">
-        <Link to="/admin">
-          <button>관리자 메뉴</button>
-        </Link>
-      </div>
-      <div className="product_grid">
-        {models.map((m, i) => {
-          return <Product {...m}></Product>
-        })}
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="products">
+            <div className="products_admin">
+                <Link to="/admin">
+                    <button>관리자 메뉴</button>
+                </Link>
+            </div>
+            <div className="product_grid">
+                {models.map((m, i) => {
+                    return <Product {...m}></Product>;
+                })}
+            </div>
+        </div>
+    );
+};
